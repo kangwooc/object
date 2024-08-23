89f6903ac2d4d3dd7f06f0fc6bb709078f248ca8
Kangwoo choi <kangwooc@gmail.com>
2024-08-23 22:55:20 +0900

    feat: 개선 전 코드

---
+++ src/main/java/chapter1/Theater.java
@@ -0,0 +1,23 @@
+package chapter1;
+
+import java.util.List;
+
+public class Theater {
+    private TicketSeller ticketSeller;
+
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+
+    public void enter(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().setTicket(ticket);
+            return;
+        }
+        Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+        audience.getBag().minusAmount(ticket.getFee());
+        ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+        audience.getBag().setTicket(ticket);
+    }
+}
