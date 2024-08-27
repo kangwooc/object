701f389bd85959db372fab1b7cf56bfe599878b1
Kangwoo choi <kangwooc@gmail.com>
2024-08-27 22:31:56 +0900

    할인 정책을 합성으로 만드는 interface 추가

---
+++ src/main/java/chapter5/AmountDiscountMovie.java
@@ -0,0 +1,18 @@
+package chapter5;
+
+import java.time.Duration;
+import java.util.List;
+
+public class AmountDiscountMovie extends Movie {
+    private Money discountAmount;
+
+    public AmountDiscountMovie(String title, Duration runningTime, Money fee, List<DiscountCondition> discountConditions, Money discountAmount) {
+        super(title, runningTime, fee, discountConditions);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money calculateDiscountAmount() {
+        return discountAmount;
+    }
+}
--- src/main/java/chapter5/Movie.java
+++ src/main/java/chapter5/Movie.java
@@ -1,20 +1,20 @@
 package chapter5;
 
-
-import chapter4.MovieType;
-
 import java.time.Duration;
 import java.util.List;
 
-public class Movie {
+public abstract class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
     private List<DiscountCondition> discountConditions;
 
-    private MovieType movieType;
-    private Money discountAmount;
-    private Money discountPercent;
+    public Movie(String title, Duration runningTime, Money fee, List<DiscountCondition> discountConditions) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountConditions = discountConditions;
+    }
 
     public Money calculateMovieFee(Screening screening) {
         if (isDiscountable(screening)) {
@@ -23,27 +23,14 @@ public class Movie {
         return fee;
     }
 
-    private Money calculateDiscountAmount() {
-        switch (movieType) {
-            case AMOUNT_DISCOUNT:
-                return calculateDiscountAmount();
-            case PERCENT_DISCOUNT:
-                return calculatePercentDiscountAmount();
-            case NONE_DISCOUNT:
-                return calculateNoneDiscountAmount();
-        }
-        throw new IllegalStateException();
-    }
-
-    private Money calculateNoneDiscountAmount() {
-        return Money.ZERO;
-    }
-
-    private Money calculatePercentDiscountAmount() {
-        return fee.minus(discountPercent);
-    }
     // 다형성을 통해 응집도 문제 해결
     private boolean isDiscountable(Screening screening) {
         return discountConditions.stream().anyMatch(discountCondition -> discountCondition.isSatisfiedBy(screening));
     }
+
+    protected Money getFee() {
+        return fee;
+    }
+
+    abstract protected Money calculateDiscountAmount();
 }
